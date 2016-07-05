6042063a1e3d62fb5bb117f4d54f2d741c6da6d2
codedai <wangjiahao@foxmail.com>
2016-07-05 09:59:42 +0800

    Revert "hh"
    This reverts commit ca3e0d2eabd39ab15efcb134878c21a9c6ca2b3d.

--- src/main/java/scu/controller/Controller.java
+++ src/main/java/scu/controller/Controller.java
@@ -1,6 +1,6 @@
 package scu.controller;
 
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
