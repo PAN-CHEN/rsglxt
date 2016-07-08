d26f5190e01f22631bbcb09797399654b9157826
lijiankuan <j.k.lijiankuan@outlook.com>
2016-07-08 14:39:27 +0800

    404页面错误处理
    添加了404的错误处理

--- src/main/java/scu/config/WebMvcConfig.java
+++ src/main/java/scu/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package scu.config;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import scu.Interceptor.LoginInterceptor;
@@ -11,6 +15,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/**").excludePathPatterns("/login");
+                .addPathPatterns("/**").excludePathPatterns("/login").excludePathPatterns("/error/*");
+    }
+
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
+//            container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/403"));
+        };
     }
 }
